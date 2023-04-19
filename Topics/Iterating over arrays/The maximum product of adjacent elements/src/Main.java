e34d34ca35c05dccdf665bd97de0e4e0ba4220c0
BogdanKaliuzhniy <kaliuzhnb@gmail.com>
2023-04-19 16:04:59 +0300

    The maximum product of adjacent elements

---
+++ Topics/Iterating over arrays/The maximum product of adjacent elements/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] array = new int[sc.nextInt()];
+        int max = 0;
+        int current;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        for (int i = array.length - 1; i > 0; i--) {
+            current = array[i] * array[i - 1];
+            if (max < current) {
+                max = current;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
